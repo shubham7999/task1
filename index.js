55e516dbcb77b5392a15168fab9ebf2afe0fae24
Shubham <shubhampokhriyal2018@gmail.com>
2022-06-22 14:18:58 +0530

    added mongoose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const  express = require('express');
 const  app = express();
 const bodyParser = require('body-parser');
 const port = 8000;
-const Chat = require("./models/chat");
+const Chat = require("./model/chat");
 //database part
 const mongoose = require('mongoose');
 
