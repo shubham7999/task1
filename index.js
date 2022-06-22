944886437d54ae3265ba016321050097df31a158
Shubham <shubhampokhriyal2018@gmail.com>
2022-06-22 14:09:40 +0530

    added post route

--- index.js
+++ index.js
@@ -31,3 +31,34 @@ app.get('/', function (req, res) {
     return res.send("Hello !!");
  })
 
+
+  //=======================================================================================================================================
+ // Posting into database the message of a partciular userr
+ app.post('/chatlog/addchat/:id' , async (req , res)=>{
+      
+    console.log(req.body);
+
+    if (!req.body.message) return res.send(400, "Invalid message please write some message");
+    if (!req.body.isSent && isSent != "True" && isSent != "False")  return res.send(400, "Invalid isSent value");
+
+
+    try{
+
+        var new_chat = new Chat({
+        username : req.params.id,
+        message: req.body.message,
+        isSent:req.body.isSent
+    })
+     
+    const chatid = await new_chat.save();
+    res.send(200 , chatid._id);
+
+
+}catch(e){
+   
+    console.error("this is error=>", e.message)
+    res.send(500 , 'Server error');
+}
+     
+ })
+
